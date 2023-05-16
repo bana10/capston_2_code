f3eee667d216cdb68eef5bc74572c32625d10c60
lee ji hyeon <marosemerry1097@gmail.com>
2023-05-17 08:43:16 +0900

    현재 위치에서 대각선으로 이동할 수 있도록 코드 수정.

--- ras_2_moter_2.py
+++ ras_2_moter_2.py
@@ -1,6 +1,7 @@
 import serial
 import RPi.GPIO as GPIO
 import time
+import math
 
 ser = serial.Serial('/dev/ttyS0', 115200) # 시리얼 포트 설정
 
@@ -48,6 +49,26 @@ def calculate_angle(center):
     angle = int((center[0] / 640) * 180)
     return angle
 
+def get_current_motor_position():
+    # PWM 신호의 duty cycle을 이용하여 모터의 현재 위치를 계산하고 반환
+    duty_cycle_x = pwm_x.get_duty_cycle()
+    duty_cycle_y = pwm_y.get_duty_cycle()
+
+    # 현재 위치를 duty cycle 값에서 위치로 변환하는 계산식을 사용하여 계산
+    position_x = (duty_cycle_x - DUTY_CYCLE_MIN) / (DUTY_CYCLE_MAX - DUTY_CYCLE_MIN) * (max_x - min_x) + min_x
+    position_y = (duty_cycle_y - DUTY_CYCLE_MIN) / (DUTY_CYCLE_MAX - DUTY_CYCLE_MIN) * (max_y - min_y) + min_y
+
+    return position_x, position_y
+
+def calculate_distance(point1, point2):
+    # 두 점 사이의 거리를 계산
+    x1, y1 = point1
+    x2, y2 = point2
+
+    distance = math.sqrt((x2 - x1)**2 + (y2 - y1)**2)
+    return distance
+
+
 if __name__ == "__main__":
     # 중심점 좌표를 저장하는 스택 초기화
     centers = []
@@ -72,21 +93,47 @@ if __name__ == "__main__":
                 min_center = sorted_centers[0]
                 max_center = sorted_centers[-1]
                 # 가장 외곽의 두 점의 위치가 변경되었는지 확인한다
-                if min_center != sorted_centers[0] or max_center != sorted_centers[-1]:
-                   # 양 끝점이 정렬된 것과 다른 경우 x축은 정렬전 배열의 각 끝점을 각도로 환산한 만큼, y축은 정렬 양 끝값 평균값
-                    angle1 = calculate_angle(sorted_centers[0])
-                    angle2 = calculate_angle(sorted_centers[-1])
-                    x_distance = abs(sorted_centers[-1][0] - sorted_centers[0][0])
+                if ((min_center == centers[0] and max_center == centers[-1]) or (min_center == centers[-1] and max_center == centers[0]))!=1:
+                   # 양 끝점이 정렬된 것과 다른 경우 x축은 정렬전 배열의 각 끝점을 각도로 환산한 만큼, 
+                   # y축은 정렬 양 끝값 평균값
+                    angle1 = calculate_angle(centers[0])
+                    angle2 = calculate_angle(centers[-1])
+                    x_distance = abs(centers[-1][0] - centers[0][0])
                     x_angle = calculate_angle((0, 0), (x_distance, 0)) 
-                    move_motors(angle1 + x_angle, (sorted_centers[0][1]+sorted_centers[-1][1])/2)
+                    move_motors(angle1 + x_angle, (sorted_centers[0][1] + sorted_centers[-1][1]) / 2)
+
                 else:
-                    # 가장 작은 점과 가장 큰 점 사이의 대각선을 따라 모터를 이동한다
+                    # 현재 모터의 위치
+                    current_position = get_current_motor_position() 
+                   # 가장 작은 점과 가장 큰 점 사이의 대각선을 따라 모터를 이동한다
                     median_center = sorted_centers[len(sorted_centers) // 2]
                     left_end, right_end = sorted_centers[0], sorted_centers[-1]
-                    if median_center[0] < left_end[0]:
-                        angle1 = calculate_angle(median_center, left_end)
+
+                    # 현재 위치와 가까운 쪽 방향으로 우선 대각선으로 이동
+                    if calculate_distance(current_position, left_end) < calculate_distance(current_position, right_end):
+                        angle1 = calculate_angle(current_position, left_end)
                         angle2 = calculate_angle(median_center, right_end)
                     else:
-                        angle1 = calculate_angle(median_center, right_end)
+                        angle1 = calculate_angle(current_position, right_end)
                         angle2 = calculate_angle(median_center, left_end)
+
                     move_motors(angle1, angle2)
+
+                    
+
+
+                    """
+                    1. sorted_centers 리스트에서 중앙에 있는 점인 median_center를 찾음. 
+
+                    2. sorted_centers의 첫 번째와 마지막 점을 각각 left_end와 right_end에 할당.
+                    
+                    3. median_center의 x 좌표와 left_end의 x 좌표를 비교하여 각도를 계산합니다. 
+                    만약 median_center의 x 좌표가 left_end의 x 좌표보다 작다면, 
+                    angle1은 median_center와 left_end 사이의 각도가 되고, 
+                    angle2는 median_center와 right_end 사이의 각도가 됩니다. 
+
+                    그렇지 않으면 angle1은 median_center와 right_end 사이의 각도가 되고, 
+                    angle2는 median_center와 left_end 사이의 각도가 됩니다.
+                    
+                    4. 계산된 angle1과 angle2를 이용하여 모터를 제어합니다. 
+                    """
