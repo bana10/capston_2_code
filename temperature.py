3722afe06b4cde83f48a5b8ec5ff87cd9fe6d91c
lee ji hyeon <marosemerry1097@gmail.com>
2023-05-31 07:19:34 +0900

    dht는 잘 작동되는데 다른게 잘 안되서 계속 실험중

--- temperature.py
+++ temperature.py
@@ -4,67 +4,55 @@ import numpy as np
 import RPi.GPIO as GPIO
 import Adafruit_DHT
 
-
-
-
 # DTS-L300-V2 센서 설정
-DTSPin = [27, 22, 23, 24, 25, 26]
-'''
-SCK : GPIO 핀 25
-SDI : GPIO 핀 24
-SDD : GPIO 핀 23
-SCE : GPIO 핀 22
-'''
-
+DTSPin = {
+    'SCK': 25,
+    'SDI': 24,
+    'SDD': 23,
+    'SCE': 22
+}
 
 # DHT11 센서 설정
 DHT_PIN = 18
 DHT_TYPE = Adafruit_DHT.DHT11
 
-def read_temperature(DTSPin):
+def setup_dts():
     GPIO.setmode(GPIO.BCM)
-    for i in range(6):
-        GPIO.setup(DTSPin[i], GPIO.OUT)
-        GPIO.output(DTSPin[i], GPIO.LOW)
+    for pin in DTSPin.values():
+        GPIO.setup(pin, GPIO.OUT)
+        GPIO.output(pin, GPIO.LOW)
+    GPIO.output(DTSPin['SCK'], GPIO.HIGH)
 
-    # SCK 핀을 HIGH로 설정
-    GPIO.output(DTSPin[4], GPIO.HIGH)
-
-    # 16비트 데이터 읽기
+def read_temperature_dts():
     data = 0
-    for i in range(16):
-        # SCK 핀을 LOW로 설정
-        GPIO.output(DTSPin[4], GPIO.LOW)
-
-        # SDD 핀에서 데이터 읽기
-        bit = GPIO.input(DTSPin[3])
-
-        # 데이터를 1비트씩 왼쪽으로 시프트하고 읽은 비트를 맨 오른쪽에 추가
+    for _ in range(16):
+        GPIO.output(DTSPin['SCK'], GPIO.LOW)
+        bit = GPIO.input(DTSPin['SDI'])
         data = (data << 1) | bit
+        GPIO.output(DTSPin['SCK'], GPIO.HIGH)
 
-        # SCK 핀을 HIGH로 설정
-        GPIO.output(DTSPin[4], GPIO.HIGH)
-
-    # 온도 계산
     if data & 0x8000:
-        # 음수 온도인 경우
-        temperature = -((data ^ 0xFFFF) + 1) / 16.0
+        temperature = -((data ^ 0xFFFF) + 1) / 16.0  # 음수 온도인 경우
     else:
-        # 양수 온도인 경우
-        temperature = data / 16.0
+        temperature = data / 16.0  # 양수 온도인 경우
 
     return temperature
 
-
 if __name__ == "__main__":
-    while True:
-        # DHT11 
-        humidity, temperature = Adafruit_DHT.read_retry(DHT_TYPE, DHT_PIN)
-        print("Temperature: {}, Humidity: {}\n".format(temperature, humidity))
+    GPIO.setwarnings(False)
+    try:
+        setup_dts()
+        while True:
+            # DHT11
+            humidity, temperature_dht = Adafruit_DHT.read_retry(DHT_TYPE, DHT_PIN)
+            print("Temperature (DHT11): {}, Humidity: {}".format(temperature_dht, humidity))
 
-        # DTS-L300-V2 
-        temperature_dts = read_temperature(DTSPin)
-        print("Temperature: {}\n".format(temperature_dts))
+            # DTS-L300-V2
+            temperature_dts = read_temperature_dts()
+            print("Temperature (DTS-L300-V2): {}".format(temperature_dts))
 
-        time.sleep(1)
-
+            time.sleep(1)
+    except KeyboardInterrupt:
+        print("Program stopped by user")
+    finally:
+        GPIO.cleanup()
