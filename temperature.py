b3f3697c5c81be87ef111661b28e408b1a27b44c
lee ji hyeon <marosemerry1097@gmail.com>
2023-05-31 08:31:12 +0900

    센서 모듈 산 곳의 코드를 걍 그대로 복 붙함

--- temperature.py
+++ temperature.py
@@ -18,51 +18,45 @@ DTSPin = {
 DHT_PIN = 18
 DHT_TYPE = Adafruit_DHT.DHT11
 
-# GPIO 초기화
-GPIO.setwarnings(False)
-GPIO.setmode(GPIO.BCM)
-GPIO.setup(DTSPin['SCE'], GPIO.OUT)
-GPIO.output(DTSPin['SCE'], GPIO.HIGH)
+SPEED_1MHz = 1000000  # SPI Speed
+SPI_MODE3 = 3  # SPI MODE
+OBJECT = 0xA0  # COMMAND(Read Object Temp.)
+SENSOR = 0xA1  # COMMAND(Read Sensor Temp.)
 
 spi = spidev.SpiDev()
 spi.open(0, 0)
-spi.max_speed_hz = 1000000  # SPI 통신 속도 설정 (1MHz)
+spi.max_speed_hz = SPEED_1MHz
+spi.mode = SPI_MODE3
 
-# DTS-L300-V2 센서 초기화 함수
-def initialize_sensor():
-    spi.xfer2([0x01])  # Initialization 명령어 전송
-    time.sleep(0.05)  # 초기화 시간 대기
+GPIO.setwarnings(False)
+GPIO.setmode(GPIO.BCM)
+GPIO.setup(DTSPin['SCE'], GPIO.OUT)
+GPIO.output(DTSPin['SCE'], 1)
 
-def read_temperature():
-    # 센서에서 데이터 읽기
-    spi.xfer2([0x01])  # Start Conversion 명령어 전송
-    time.sleep(0.05)  # 변환이 완료될 때까지 대기
 
-    spi.xfer2([0x00])  # Read Data 명령어 전송
-    time.sleep(0.05)  # 데이터 읽기 대기
+def spi_command(adr):
+    data_buf = [adr, 0x22, 0x22]
 
-    resp = spi.xfer2([0x00, 0x00])  # 데이터 수신
+    GPIO.output(DTSPin['SCE'], 0)
+    time.sleep(0.00001)
 
-    # 온도 값 계산
-    raw_temp = ((resp[0] & 0x7F) << 8) + resp[1]
-    if resp[0] & 0x80:
-        temp = -((raw_temp ^ 0x7FFF) + 1) * 0.02
-    else:
-        temp = raw_temp * 0.02
+    resp = spi.xfer2(data_buf)
 
-    return temp
+    GPIO.output(DTSPin['SCE'], 1)
 
+    return (resp[2] * 256 + resp[1])
 
 if __name__ == "__main__":
-    initialize_sensor()
+
     while True:
         # DHT11
         humidity, temperature_dht = Adafruit_DHT.read_retry(DHT_TYPE, DHT_PIN)
         print("Temperature (DHT11): {}, Humidity: {}".format(temperature_dht, humidity))
 
         # DTS-L300-V2
-        temperature_dts = read_temperature()
-        print("Temperature: {:.2f} °C".format(temperature_dts))
+        iSensor = spi_command(SENSOR)
+        iObject = spi_command(OBJECT)
+        print("Sensor: {:.2f}, Object: {:.2f}".format(iSensor/100, iObject/100))
         time.sleep(1)
 
-    GPIO.cleanup()
+        GPIO.cleanup()
