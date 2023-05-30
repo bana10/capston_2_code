1eaa3eab77f23be7f2dbcacbddf7ab9ece4a7a28
lee ji hyeon <marosemerry1097@gmail.com>
2023-05-31 07:52:18 +0900

    no message

--- temperature.py
+++ temperature.py
@@ -16,43 +16,47 @@ DTSPin = {
 DHT_PIN = 18
 DHT_TYPE = Adafruit_DHT.DHT11
 
-def setup_dts():
-    GPIO.setmode(GPIO.BCM)
-    for pin in DTSPin.values():
-        GPIO.setup(pin, GPIO.OUT)
-        GPIO.output(pin, GPIO.LOW)
-    GPIO.output(DTSPin['SCK'], GPIO.HIGH)
+# GPIO 초기화
+GPIO.setwarnings(False)
+GPIO.setmode(GPIO.BCM)
+GPIO.setup(DTSPin['SCK'], GPIO.OUT)
+GPIO.setup(DTSPin['SDI'], GPIO.OUT)
+GPIO.setup(DTSPin['SDD'], GPIO.IN)
+GPIO.setup(DTSPin['SCE'], GPIO.OUT)
 
-def read_temperature_dts():
+
+def read_temperature():
+    # 센서에서 데이터 읽기
     data = 0
-    for _ in range(16):
-        GPIO.output(DTSPin['SCK'], GPIO.LOW)
-        bit = GPIO.input(DTSPin['SDI'])
-        data = (data << 1) | bit
+
+    GPIO.output(DTSPin['SCE'], GPIO.LOW)
+
+    for i in range(16):
         GPIO.output(DTSPin['SCK'], GPIO.HIGH)
+        time.sleep(0.001)
+        bit = GPIO.input(DTSPin['SDD'])
+        data |= (bit << (15 - i))
+        GPIO.output(DTSPin['SCK'], GPIO.LOW)
+        time.sleep(0.001)
 
-    if data & 0x8000:
-        temperature = -((data ^ 0xFFFF) + 1) / 16.0  # 음수 온도인 경우
-    else:
-        temperature = data / 16.0  # 양수 온도인 경우
+    GPIO.output(DTSPin['SCE'], GPIO.HIGH)
 
-    return temperature
+    # 온도 값 계산
+    temp = ((data & 0xFF00) >> 8) + ((data & 0x00FF) / 256.0)
+    temp = temp * 0.0625
+
+    return temp
 
 if __name__ == "__main__":
     GPIO.setwarnings(False)
-    try:
-        setup_dts()
-        while True:
-            # DHT11
-            humidity, temperature_dht = Adafruit_DHT.read_retry(DHT_TYPE, DHT_PIN)
-            print("Temperature (DHT11): {}, Humidity: {}".format(temperature_dht, humidity))
+    while True:
+        # DHT11
+        humidity, temperature_dht = Adafruit_DHT.read_retry(DHT_TYPE, DHT_PIN)
+        print("Temperature (DHT11): {}, Humidity: {}".format(temperature_dht, humidity))
 
-            # DTS-L300-V2
-            temperature_dts = read_temperature_dts()
-            print("Temperature (DTS-L300-V2): {}".format(temperature_dts))
+        # DTS-L300-V2
+        temperature_dts = read_temperature()
+        print("Temperature: {:.2f} °C".format(temperature_dts))
+        time.sleep(1)
 
-            time.sleep(1)
-    except KeyboardInterrupt:
-        print("Program stopped by user")
-    finally:
         GPIO.cleanup()
