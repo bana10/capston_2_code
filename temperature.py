cd2e879a6696a03ebe2b4ba5cd3221f29ee132c3
superppapjwh <jwhgogogo82@naver.com>
2023-06-06 17:02:05 +0900

    wasdsad

--- temperature.py
+++ temperature.py
@@ -23,16 +23,17 @@ def RC_Motor(angle):
 #2.5의 값은 서보 모터의 펄스 폭 범위와 스케일링 계수를 기준으로 선택됩니다. 예를 들어, 서보 모터의 회전 범위가 -90~90도일 때 펄스 폭 범위가 600~2400마이크로초인 경우, 이는 50Hz PWM 신호에 대해 약 3~12%의 듀티 사이클 범위에 해당합니다. 스케일링된 각도 값에 2.5를 더하면 최소 듀티 사이클이 3%로 설정됩니다 래. 근데 블로그 설명에서 보면 저거 펄스폭 범위가 600~2400마이크로초야. 
     pwm.ChangeDutyCycle(duty_cycle)
 
-
+RC_Motor(0)
+time.sleep(10)
 while True:
     RC_Motor(-90)
-    time.sleep(0.5)
+    time.sleep(1)
     RC_Motor(-45)
-    time.sleep(0.5)
+    time.sleep(1)
     RC_Motor(0)
-    time.sleep(0.5)
+    time.sleep(1)
     RC_Motor(45)
-    time.sleep(0.5)
+    time.sleep(1)
     RC_Motor(90)
-    time.sleep(0.5)
+    time.sleep(1)
 
