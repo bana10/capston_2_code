798c11d6adaf790e23c55564017e584f3692b779
lee ji hyeon <marosemerry1097@gmail.com>
2023-06-06 17:01:27 +0900

    no message

--- FAN_MOTER.py
+++ FAN_MOTER.py
@@ -14,7 +14,6 @@ GPIO.setmode(GPIO.BCM)
 GPIO.setup(ENA, GPIO.OUT)
 GPIO.setup(IN1, GPIO.OUT)
 
-
 # PWM 객체 생성
 pwm = GPIO.PWM(ENA, 100)  # 100Hz의 PWM 주파수
 
@@ -23,24 +22,28 @@ def motor_control(direction, speed):
     GPIO.output(IN1, direction)
     pwm.start(speed)
 
+# 모터 정지 함수
+def stop_motor():
+    GPIO.output(IN1, GPIO.LOW)
+    pwm.stop()
 
 # 모터 속도를 계산하는 함수
 def calculate_speed(humidity, temperature, distance):
-    wind_speed = 0.42 * (math.sqrt(temperature)-23.8)*((100-humidity)/10)*math.pow((distance/10),-0.2)
+    wind_speed = 0.42 * (math.sqrt(temperature) - 23.8) * ((100 - humidity) / 10) * math.pow((distance / 10), -0.2)
     return wind_speed
-
 
 try:
     while True:
-        humidity,temperature = Adafruit_DHT.read_retry(DHT_TYPE, DHT_PIN)
+        humidity, temperature = Adafruit_DHT.read_retry(DHT_TYPE, DHT_PIN)
         print("Temperature: {}, Humidity: {}".format(temperature, humidity))
-        speed = calculate_speed(humidity, temperature,80)
-        print("속도  : %f",speed*1000)
-        motor_control(GPIO.HIGH, speed * 1000)
-        time.sleep(10)  
-
+        speed = calculate_speed(humidity, temperature, 80)
+        print(speed)
+        print("속도: {}".format(speed * 100))
+        motor_control(GPIO.HIGH, speed * 100)
+        time.sleep(10)
 
 except KeyboardInterrupt:
+    stop_motor()
     GPIO.cleanup()
 
 GPIO.cleanup()
