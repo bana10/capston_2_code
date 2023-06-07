e3bc6cfaadd5ea81e710637aa87542131f3f9824
superppapjwh <jwhgogogo82@naver.com>
2023-06-07 17:27:45 +0900

    asdfaf

--- ras1hog.py
+++ ras1hog.py
@@ -3,6 +3,12 @@ import cv2
 import time
 import RPi.GPIO as GPIO
 
+import joblib
+import sklearn
+
+x_model = joblib.load('./Xloc_model.pkl')
+y_model = joblib.load('./Yloc_model.pkl')
+
 
 cap = cv2.VideoCapture(0)
 prevt = time.time()
@@ -21,6 +27,9 @@ y_pwm = GPIO.PWM(y_servo_pin, 50)
 
 x_pwm.start(0)
 y_pwm.start(0)
+
+prev_x = 0
+prev_y = 0
 
 
 def X_Motor(angle):
@@ -69,6 +78,20 @@ try:
                 center_x = x + int(w / 2)
                 center_y = y + int(h / 2)
 
+                if len(boxes) == 1:
+                    if prev_x != 0 and prev_y != 0:
+                        dat = [[prev_x, prev_y, center_x, center_y]]
+                        p_x = int(x_model.predict(dat))
+                        p_y = int(y_model.predict(dat))
+                        prev_x = center_x
+                        prev_y = center_y
+
+                        center_x = p_x
+                        center_y = p_y
+                    else:
+                        prev_y = center_y
+                        prev_x = center_x
+
                 if center_x > maxx:
                     maxx = center_x
                 if center_x < minx:
@@ -77,6 +100,9 @@ try:
                     maxy = center_y
                 if center_y < miny:
                     miny = center_y
+            
+            
+
 
             minanglex = int((minx / wp * (Xspin * 2)) - Xspin)
             maxanglex = int((maxx / wp * (Xspin * 2)) - Xspin)
@@ -98,12 +124,15 @@ try:
                 Y_Motor(60)
             print("avgy:", yangleavg)
             goleft = not goleft
+        else:
+            prev_x = 0
+            prev_y = 0
 
-        cv2.imshow('asdf', frame)
-        if cv2.waitKey(1) == ord('q'):
-            X_Motor(0)
-            GPIO.cleanup()
-            break
+        # cv2.imshow('asdf', frame)
+        # if cv2.waitKey(1) == ord('q'):
+        #     X_Motor(0)
+        #     GPIO.cleanup()
+        #     break
 
         print(time.time() - prevt)
         prevt = time.time()
