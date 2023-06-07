d8bcb381f01410d9424b20e46078b769da730767
superppapjwh <jwhgogogo82@naver.com>
2023-06-07 17:57:47 +0900

    teqe

---
+++ t2.py
@@ -0,0 +1,36 @@
+import RPi.GPIO as GPIO
+import time
+
+
+servo_pin = 13  # GPIO pin connected to the servo motor
+servo_pin2 = 19  # GPIO pin connected to the servo motor
+GPIO.setmode(GPIO.BCM)
+GPIO.setup(servo_pin, GPIO.OUT)
+GPIO.setup(servo_pin2, GPIO.OUT)
+
+pwm = GPIO.PWM(servo_pin, 50)
+pwm2 = GPIO.PWM(servo_pin2, 50)
+# 50 Hz PWM Frequency 이게 hs-311모터 기본 주파수. #데이터시트에 적혀있어
+pwm.start(0)
+pwm2.start(0)
+
+
+def RC_Motor(angle, a2):
+    # angle : -90도 ~ +90도
+    if angle < -90:
+        angle = -90
+    if angle > 90:
+        angle = 90
+    duty_cycle = (angle + 90) / 18 + 2.5
+    dc2 = (a2+90) / 18 + 2.5
+    pwm.ChangeDutyCycle(duty_cycle)
+    pwm2.ChangeDutyCycle(dc2)
+
+
+RC_Motor(-90, 9)
+time.sleep(1)
+RC_Motor(90, 90)
+time.sleep(1)
+RC_Motor(0, 90)
+time.sleep(1)
+GPIO.cleanup()
